Black-Tek/BlackTek-Server

commit 0da6263793355166765cfba7cd4f09144584b1a3

Added Reward Container & Boss System (#14)
-Added Reward Container
-Added Unique Loot
-Created Boss Type monsters
-Boss loot based on participation
-Added a Reward Chest, and placed on map
-Add configurations in config.lua

src/game.cpp
@@ -1116,6 +1116,18 @@ ReturnValue Game::internalMoveItem(Cylinder* fromCylinder, Cylinder* toCylinder,
 		return RETURNVALUE_NOERROR; //silently ignore move
 	}
 
+	if (Container* toContainer = dynamic_cast<Container*>(toCylinder)) {
+		if (toContainer->isRewardCorpse() || toContainer->getID() == ITEM_REWARD_CONTAINER) {
+			return RETURNVALUE_NOTPOSSIBLE;
+		}
+	}
+
+	if (Container* itemContainer = dynamic_cast<Container*>(item)) {
+		if (itemContainer->isRewardCorpse() || item->getID() == ITEM_REWARD_CONTAINER) {
+			return RETURNVALUE_NOERROR; // silently ignore move
+		}
+	}
+
 	//check if we can add this item
 	ReturnValue ret = toCylinder->queryAdd(index, *item, count, flags, actor);
 	if (ret == RETURNVALUE_NEEDEXCHANGE) {
@@ -4097,6 +4109,22 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 		int32_t realHealthChange = target->getHealth();
 		target->gainHealth(attacker, damage.primary.value);
 		realHealthChange = target->getHealth() - realHealthChange;
+		
+		// rewardboss healing contribution
+		if (target && target->getPlayer()) {
+			for (const auto& [monsterId, rewardInfo] : g_game.rewardBossTracking) {
+				Monster* monster = getMonsterByID(monsterId);
+				if (monster && monster->isRewardBoss()) {
+					const Position& targetPos = target->getPosition();
+					const Position& monsterPos = monster->getPosition();
+					double distBetweenTargetAndBoss = std::sqrt(std::pow(targetPos.x - monsterPos.x, 2) + std::pow(targetPos.y - monsterPos.y, 2));
+					if (distBetweenTargetAndBoss < 7) {
+						uint32_t playerGuid = target->getPlayer()->getGUID();
+						rewardBossTracking[monsterId].playerScoreTable[playerGuid].damageTaken += realHealthChange * g_config.getFloat(ConfigManager::REWARD_RATE_HEALING_DONE);
+					}
+				}
+			}
+		}
 
 		if (realHealthChange > 0 && !target->isInGhostMode()) {
 			auto damageString = fmt::format("{:d} hitpoint{:s}", realHealthChange, realHealthChange != 1 ? "s" : "");
@@ -4337,6 +4365,34 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 					message.text = spectatorMessage;
 				}
 				tmpPlayer->sendTextMessage(message);
+			}
+		}
+
+		// rewardboss player attacking boss
+		if (target && target->getMonster() && target->getMonster()->isRewardBoss()) {
+			uint32_t monsterId = target->getMonster()->getID();
+
+			if (rewardBossTracking.find(monsterId) == rewardBossTracking.end()) {
+				rewardBossTracking[monsterId] = RewardBossContributionInfo();
+			}
+
+			if (attacker->getPlayer()) {
+				uint32_t playerGuid = attacker->getPlayer()->getGUID();
+				rewardBossTracking[monsterId].playerScoreTable[playerGuid].damageDone += realDamage * g_config.getFloat(ConfigManager::REWARD_RATE_DAMAGE_DONE);
+			}
+		}
+
+		// rewardboss boss attacking player
+		if (attacker && attacker->getMonster() && attacker->getMonster()->isRewardBoss()) {
+			uint32_t monsterId = attacker->getMonster()->getID();
+
+			if (rewardBossTracking.find(monsterId) == rewardBossTracking.end()) {
+				rewardBossTracking[monsterId] = RewardBossContributionInfo();
+			}
+
+			if (target->getPlayer()) {
+				uint32_t playerGuid = target->getPlayer()->getGUID();
+				rewardBossTracking[monsterId].playerScoreTable[playerGuid].damageTaken += realDamage * g_config.getFloat(ConfigManager::REWARD_RATE_DAMAGE_TAKEN);
 			}
 		}
 
@@ -5908,4 +5964,9 @@ bool Game::reload(ReloadTypes_t reloadType)
 		}
 	}
 	return true;
+}
+
+void Game::resetDamageTracking(uint32_t monsterId)
+{
+	rewardBossTracking.erase(monsterId);
 }

config.lua.dist
@@ -143,6 +143,13 @@ rateLoot = 2
 rateMagic = 3
 rateSpawn = 1
 
+-- Rates for reward boss system
+-- If a player meets their share of the expectedScore (totalScore/contributors), they receive a lootRate according to the rewardBaseRate.
+rewardBaseRate = 1
+rewardRateDamageDone = 1
+rewardRateDamageTaken = 0.5
+rewardRateHealingDone = 0.3
+
 -- Monster Despawn Config
 -- despawnRange is the amount of floors a monster can be from its spawn position
 -- despawnRadius is how many tiles away it can be from its spawn position

data/creaturescripts/scripts/login.lua
@@ -24,6 +24,18 @@ function onLogin(player)
 	elseif not promotion then
 		player:setVocation(vocation:getDemotion())
 	end
+	
+	-- Notify player about the rewards in their reward chest
+	local rewardChest = player:getRewardChest()
+	local rewardContainerCount = 0
+	for _, item in ipairs(rewardChest:getItems()) do
+		if item:getId() == ITEM_REWARD_CONTAINER then
+			rewardContainerCount = rewardContainerCount + 1
+		end
+	end
+	if rewardContainerCount > 0 then
+		player:sendTextMessage(MESSAGE_STATUS_DEFAULT, string.format("You have %d reward%s in your reward chest.", rewardContainerCount, rewardContainerCount > 1 and "s" or ""))
+	end
 
 	-- Events
 	player:registerEvent("PlayerDeath")

data/items/items.xml
@@ -31349,6 +31349,7 @@
         <attribute key="description" value="It is frosted with tiny ice crystals and starts to melt as look at it." />
     </item>
     <item id="21584" article="a" name="reward chest">
+		<attribute key="type" value="rewardchest" />
         <attribute key="description" value="This chest contains your rewards earned in battles." />
         <attribute key="containersize" value="32" />
         <attribute key="pickupable" value="0" />

data/migrations/30.lua
@@ -1,3 +1,16 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 30 (reward boss system)")
+	db.query([[
+		CREATE TABLE IF NOT EXISTS `player_rewarditems` (
+		  `player_id` int NOT NULL,
+		  `sid` int NOT NULL COMMENT 'range 0-100 will be reserved for adding items to player who are offline and all > 100 is for items saved from reward chest',
+		  `pid` int NOT NULL DEFAULT '0',
+		  `itemtype` smallint unsigned NOT NULL,
+		  `count` smallint NOT NULL DEFAULT '0',
+		  `attributes` blob NOT NULL,
+		  UNIQUE KEY `player_id_2` (`player_id`, `sid`),
+		  FOREIGN KEY (`player_id`) REFERENCES `players`(`id`) ON DELETE CASCADE
+		) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+	]])
+	return true
 end

data/migrations/31.lua
@@ -1,0 +1,3 @@
+function onUpdateDatabase()
+	return false
+end

data/monster/monsters/ferumbras.xml
@@ -16,6 +16,7 @@
 		<flag staticattack="90" />
 		<flag runonhealth="2500" />
 		<flag isboss="1" />
+		<flag isrewardboss="1" />
 	</flags>
 	<attacks>
 		<attack name="melee" interval="2000" min="0" max="-350" />
@@ -82,7 +83,7 @@
 		<voice sentence="Even in my weakened state I will crush you all!" />
 	</voices>
 	<loot>
-		<item id="5903" name="Ferumbras' hat" chance="100000" />
+		<item id="5903" name="Ferumbras' hat" chance="100000" unique="1" />
 		<item name="gold coin" countmax="100" chance="97000" />
 		<item name="gold coin" countmax="100" chance="97000" />
 		<item name="gold coin" countmax="70" chance="33000" />

commit 795533293df7358b7b86d95b108b6f32876031f4

Resolve crashes introduced from 0da6263793355166765cfba7cd4f09144584b1a3 . Which resoloves issue #22

src/game.cpp
@@ -4363,7 +4363,7 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 				rewardBossTracking[monsterId] = RewardBossContributionInfo();
 			}
 
-			if (attacker->getPlayer()) {
+			if (attacker && attacker->getPlayer()) {
 				uint32_t playerGuid = attacker->getPlayer()->getGUID();
 				rewardBossTracking[monsterId].playerScoreTable[playerGuid].damageDone += realDamage * g_config.getFloat(ConfigManager::REWARD_RATE_DAMAGE_DONE);
 			}
@@ -4377,7 +4377,7 @@ bool Game::combatChangeHealth(Creature* attacker, Creature* target, CombatDamage
 				rewardBossTracking[monsterId] = RewardBossContributionInfo();
 			}
 
-			if (target->getPlayer()) {
+			if (target && target->getPlayer()) {
 				uint32_t playerGuid = target->getPlayer()->getGUID();
 				rewardBossTracking[monsterId].playerScoreTable[playerGuid].damageTaken += realDamage * g_config.getFloat(ConfigManager::REWARD_RATE_DAMAGE_TAKEN);
 			}