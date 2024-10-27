caf89486703a3b5ad2a446ef469e66a9936471ec
jordanhoare <jordanhoare0@gmail.com>
2024-10-27 11:30:50 +0800

    feat: create profession action systems

---
+++ data/scripts/crafting/herbalist.lua
@@ -0,0 +1,318 @@
+Crafting.herbalist = {
+    --- Scissors
+    { 
+        id = 26713,
+        name = "Apprentice's Scissor",
+        level = 10,
+        cost = 1000,
+        count = 1,
+        materials = {
+            {id = 26712, count = 1},
+            {id = 26729, count = 5},
+        }
+    },
+    {
+        id = 26714,
+        name = "Herbalist's Scissor",
+        level = 15,
+        cost = 2000,
+        count = 1,
+        materials = {
+            {id = 26413, count = 1},
+            {id = 26732, count = 5},
+        }
+    },
+    {
+        id = 26715,
+        name = "Expert's Scissor",
+        level = 20,
+        cost = 3000,
+        count = 1,
+        materials = {
+            {id = 26714, count = 1},
+            {id = 26726, count = 5},
+        }
+    },
+    {
+        id = 26716,
+        name = "Master's Scissor",
+        level = 25,
+        cost = 5000,
+        count = 1,
+        materials = {
+            {id = 26715, count = 1},
+            {id = 26727, count = 5},
+        }
+    },
+    {
+        id = 28076,
+        name = "Exalted Health Potion",
+        level = 15,
+        cost = 850,
+        count = 5,
+        materials = {
+            {id = 26731, count = 3},
+            {id = 26729, count = 1},
+        }
+    },
+    {
+        id = 28076,
+        name = "Exalted Health Potion",
+        level = 15,
+        cost = 8000,
+        count = 50,
+        materials = {
+            {id = 26731, count = 30},
+            {id = 26729, count = 10},
+        }
+    },
+    {
+        id = 28080,
+        name = "Exalted Mana Potion",
+        level = 15,
+        cost = 850,
+        count = 5,
+        materials = {
+            {id = 26730, count = 3},
+            {id = 26732, count = 1},
+        }
+    },
+    {
+        id = 28080,
+        name = "Exalted Mana Potion",
+        level = 15,
+        cost = 8000,
+        count = 50,
+        materials = {
+            {id = 26730, count = 30},
+            {id = 26732, count = 10},
+        }
+    },
+    {
+        id = 28088,
+        name = "Exalted Spirit Potion",
+        level = 15,
+        cost = 850,
+        count = 5,
+        materials = {
+            {id = 26731, count = 3},
+            {id = 26732, count = 1},
+        }
+    },
+    {
+        id = 28088,
+        name = "Exalted Spirit Potion",
+        level = 15,
+        cost = 8000,
+        count = 50,
+        materials = {
+            {id = 26731, count = 30},
+            {id = 26732, count = 10},
+        }
+    },
+    {
+        id = 28077,
+        name = "Advanced Health Potion",
+        level = 20,
+        cost = 1050,
+        count = 5,
+        materials = {
+            {id = 26725, count = 3},
+            {id = 26726, count = 1},
+        }
+    },
+    {
+        id = 28077,
+        name = "Advanced Health Potion",
+        level = 20,
+        cost = 9950,
+        count = 50,
+        materials = {
+            {id = 26725, count = 30},
+            {id = 26726, count = 10},
+        }
+    },
+    {
+        id = 28081,
+        name = "Advanced Mana Potion",
+        level = 20,
+        cost = 1050,
+        count = 5,
+        materials = {
+            {id = 26727, count = 3},
+            {id = 26726, count = 1},
+        }
+    },
+    {
+        id = 28081,
+        name = "Advanced Mana Potion",
+        level = 20,
+        cost = 9950,
+        count = 50,
+        materials = {
+            {id = 26727, count = 30},
+            {id = 26726, count = 10},
+        }
+    },
+    {
+        id = 28089,
+        name = "Advanced Spirit Potion",
+        level = 20,
+        cost = 1050,
+        count = 5,
+        materials = {
+            {id = 26727, count = 3},
+            {id = 26725, count = 1},
+        }
+    },
+    {
+        id = 28089,
+        name = "Advanced Spirit Potion",
+        level = 20,
+        cost = 9950,
+        count = 50,
+        materials = {
+            {id = 26727, count = 30},
+            {id = 26725, count = 10},
+        }
+    },
+    {
+        id = 28078,
+        name = "Divine Health Potion",
+        level = 25,
+        cost = 1350,
+        count = 5,
+        materials = {
+            {id = 26723, count = 3},
+            {id = 26724, count = 1},
+        }
+    },
+    {
+        id = 28078,
+        name = "Divine Health Potion",
+        level = 25,
+        cost = 12000,
+        count = 50,
+        materials = {
+            {id = 26723, count = 30},
+            {id = 26724, count = 10},
+        }
+    },
+    {
+        id = 28082,
+        name = "Divine Mana Potion",
+        level = 25,
+        cost = 1350,
+        count = 5,
+        materials = {
+            {id = 26728, count = 3},
+            {id = 26724, count = 1},
+        }
+    },
+    {
+        id = 28082,
+        name = "Divine Mana Potion",
+        level = 25,
+        cost = 12000,
+        count = 50,
+        materials = {
+            {id = 26728, count = 30},
+            {id = 26724, count = 10},
+        }
+    },
+    {
+        id = 28090,
+        name = "Divine Spirit Potion",
+        level = 25,
+        cost = 1350,
+        count = 5,
+        materials = {
+            {id = 26723, count = 3},
+            {id = 26725, count = 1},
+        }
+    },
+    {
+        id = 28090,
+        name = "Divine Spirit Potion",
+        level = 25,
+        cost = 12000,
+        count = 50,
+        materials = {
+            {id = 26723, count = 30},
+            {id = 26725, count = 10},
+        }
+    },
+    {
+        id = 28079,
+        name = "Mythic Health Potion",
+        level = 30,
+        cost = 1650,
+        count = 5,
+        materials = {
+            {id = 26729, count = 1},
+            {id = 26732, count = 1},
+            {id = 26724, count = 1},
+        }
+    },
+    {
+        id = 28079,
+        name = "Mythic Health Potion",
+        level = 30,
+        cost = 15000,
+        count = 50,
+        materials = {
+            {id = 26729, count = 10},
+            {id = 26732, count = 10},
+            {id = 26724, count = 10},
+        }
+    },
+    {
+        id = 28083,
+        name = "Mythic Mana Potion",
+        level = 30,
+        cost = 1650,
+        count = 5,
+        materials = {
+            {id = 26729, count = 1},
+            {id = 26732, count = 1},
+            {id = 26724, count = 1},
+        }
+    },
+    {
+        id = 28083,
+        name = "Mythic Mana Potion",
+        level = 30,
+        cost = 15000,
+        count = 50,
+        materials = {
+            {id = 26729, count = 10},
+            {id = 26732, count = 10},
+            {id = 26724, count = 10},
+        }
+    },
+    {
+        id = 28091,
+        name = "Mythic Spirit Potion",
+        level = 30,
+        cost = 1650,
+        count = 5,
+        materials = {
+            {id = 26729, count = 1},
+            {id = 26732, count = 1},
+            {id = 26724, count = 1},
+        }
+    },
+    {
+        id = 28091,
+        name = "Mythic Spirit Potion",
+        level = 30,
+        cost = 15000,
+        count = 50,
+        materials = {
+            {id = 26729, count = 10},
+            {id = 26732, count = 10},
+            {id = 26724, count = 10},
+        }
+    },
+
+}
