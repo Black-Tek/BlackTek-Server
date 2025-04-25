3024df6e0c14aae53b050624958e016282132f98
Tofame <80583148+Tofame@users.noreply.github.com>
2025-04-26 01:17:29 +0200

    Add migration file

--- data/migrations/32.lua
+++ data/migrations/32.lua
@@ -1,3 +1,8 @@
 function onUpdateDatabase()
-	return false
+	print("> Updating database to version 32 (Initial Guilds Rework)")
+
+	db.query("ALTER TABLE `guild_wars` ADD COLUMN `frags_to_end` INT NOT NULL DEFAULT 30");
+	db.query("ALTER TABLE `guilds` ADD COLUMN `balance` BIGINT UNSIGNED NOT NULL DEFAULT 0");
+
+	return true
 end
