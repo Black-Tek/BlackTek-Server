4e6edc5e337bd7dd4ea48663f2404d8f93026c7a
codinablack <codinablack@gmail.com>
2025-10-03 05:50:13 -0500

    Small weapons script updates from BerntaN

--- data/scripts/weapons/distance.lua
+++ data/scripts/weapons/distance.lua
@@ -1,395 +1,395 @@
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Arbalest
 weapon:id(5803)
 weapon:level(75)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Modified crossbow
 weapon:id(8849)
 weapon:level(45)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Chain bolter
 weapon:id(8850)
 weapon:level(60)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Ironworker 
 weapon:id(8853)
 weapon:level(80)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Devileye
 weapon:id(8852)
 weapon:level(100)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Royal crossbow
 weapon:id(8851)
 weapon:level(130)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Silkweaver bow
 weapon:id(8857)
 weapon:level(40)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Composite hornbow
 weapon:id(8855)
 weapon:level(50)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Yol's bow
 weapon:id(8856)
 weapon:level(60)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Elethriel's elemental bow
 weapon:id(8858)
 weapon:level(70)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Warsinger bow
 weapon:id(8854)
 weapon:level(80)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Shimmer bow
 weapon:id(13873)
 weapon:level(40)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Hive bow
 weapon:id(15643)
 weapon:level(85)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Ornate crossbow
 weapon:id(15644)
 weapon:level(50)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Thorn spitter
 weapon:id(16111)
 weapon:level(150)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Crystal crossbow
 weapon:id(18453)
 weapon:level(90)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Mycological bow
 weapon:id(18454)
 weapon:level(105)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Triple bolt crossbow
 weapon:id(21690)
 weapon:level(70)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Icicle bow
 weapon:id(21696)
 weapon:wieldUnproperly(true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Crude umbral bow
 weapon:id(22416)
 weapon:level(75)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Umbral bow
 weapon:id(22417)
 weapon:level(150)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Umbral master bow
 weapon:id(22418)
 weapon:level(250)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Crude umbral crossbow
 weapon:id(22419)
 weapon:level(75)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Umbral crossbow
 weapon:id(22420)
 weapon:level(150)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Umbral master crossbow
 weapon:id(22421)
 weapon:level(250)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Rift bow
 weapon:id(25522)
 weapon:level(120)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Rift crossbow
 weapon:id(25523)
 weapon:level(120)
 weapon:wieldUnproperly(true)
-weapon:vocation("paladin")
+weapon:vocation("paladin", true)
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Small stone
 weapon:id(1294)
 -- breakchance: 3
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- 
 weapon:id(2111)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE)  -- Spear
 weapon:id(2389)
 -- breakchance: 3
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Throwing star
 weapon:id(2399)
 -- breakchance: 10
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Throwing knife
 weapon:id(2410)
 -- breakchance: 7
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Bolt
 weapon:id(2543)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Arrow
 weapon:id(2544)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Poison arrow
 weapon:id(2545)
 weapon:action("removecount")
 -- script: poison_arrow.lua
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Burst arrow
 weapon:id(2546)
 weapon:action("removecount")
 -- script: burst_arrow.lua
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Power bolt
 weapon:id(2547)
 weapon:level(55)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Hunting spear
 weapon:id(3965)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 -- breakchance: 6
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Infernal bolt
 weapon:id(6529)
 weapon:level(110)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Piercing bolt
 weapon:id(7363)
 weapon:level(30)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Sniper arrow
 weapon:id(7364)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Onyx arrow
 weapon:id(7365)
 weapon:level(40)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Viper star
 weapon:id(7366)
 -- script: viper_star.lua
 -- breakchance: 9
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Royal spear
 weapon:id(7367)
 weapon:level(42)
 weapon:wieldUnproperly(true)
 -- breakchance: 1
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Assassin star
 weapon:id(7368)
 weapon:level(80)
 weapon:wieldUnproperly(true)
 -- breakchance: 33
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Royal spear
 weapon:id(7378)
 weapon:level(25)
 weapon:wieldUnproperly(true)
 -- breakchance: 3
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Flash arrow
 weapon:id(7838)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Shiver arrow
 weapon:id(7839)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Flaming arrow
 weapon:id(7840)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Earth arrow
 weapon:id(7850)
 weapon:level(20)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Tarsal arrow
 weapon:id(15648)
 weapon:level(30)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Vortex bolt
 weapon:id(15649)
 weapon:level(40)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- MISSING FROM THE ITEMS.TOML
 weapon:id(18303)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Crystalline arrow
 weapon:id(18304)
 weapon:level(90)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Prismatic arrow
 weapon:id(18435)
 weapon:level(90)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Drill bolt
 weapon:id(18436)
 weapon:level(70)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Envenomed arrow
 weapon:id(18437)
 weapon:level(70)
 weapon:wieldUnproperly(true)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Glooth spear
 weapon:id(23529)
 weapon:level(60)
 weapon:wieldUnproperly(true)
 -- breakchance: 31
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Simple arrow
 weapon:id(23839)
 weapon:level(1)
 weapon:action("removecount")
 weapon:register()
 
-local weapon = Weapon(WEAPON_DISTANCE)
+local weapon = Weapon(WEAPON_DISTANCE) -- Throwing star of sula
 weapon:id(25526)
 -- breakchance: 10
 weapon:register()
