ecb431c5962680ea564b7a55e379dd509fa2f196
Silba <silba93@gmail.com>
2026-01-26 11:49:01 +0000

    fix: Player::hasAugment checking wrong augments list for items
    iterate through item->getAugments() instead of player augments for both methods

--- src/player.cpp
+++ src/player.cpp
@@ -5360,7 +5360,8 @@ const bool Player::hasAugment(const std::string_view augmentName, bool checkItem
 			{
 				if (item->isAugmented()) 
 				{
-					for (const auto& aug : augments) 
+					const auto& itemAugments = item->getAugments();
+					for (const auto& aug : *itemAugments) 
 					{
 						if (aug->getName() == augmentName) 
 						{
@@ -5391,7 +5392,8 @@ const bool Player::hasAugment(const std::shared_ptr<Augment>& augment, bool chec
 			{
                 if (item->isAugmented()) 
 				{
-					for (const auto& aug : augments) 
+					const auto& itemAugments = item->getAugments();
+					for (const auto& aug : *itemAugments) 
 					{
                         if (aug == augment) 
 						{
