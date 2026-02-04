bfbdd6a31d7081930aca3b47e1f4bb41368e4e3a
AlbertoGamer777 <76703968+AlbertoGamer777@users.noreply.github.com>
2026-02-04 17:56:07 -0400

    Two SQL delimiters changed for compatibility
    Two SQL delimiter changes have been made to be compatible with the latest version of MariaDB. This should also work on other database management systems (DELIMITER; >>>> DELIMITER ;).

--- schema.sql
+++ schema.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
 ;
 /*!40101 SET NAMES utf8mb4 */
-;
+;	
 
 --
 -- Database: `acc_manager`
@@ -125,7 +125,7 @@ CREATE TRIGGER `oncreate_guilds` AFTER INSERT ON `guilds` FOR EACH ROW BEGIN
 END
 $$
 
-DELIMITER;
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -357,7 +357,7 @@ CREATE TRIGGER `ondelete_players` BEFORE DELETE ON `players` FOR EACH ROW BEGIN
 END
 $$
 
-DELIMITER;
+DELIMITER ;
 
 -- --------------------------------------------------------
 
