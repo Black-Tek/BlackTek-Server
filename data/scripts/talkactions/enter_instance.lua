503adeb8ef02704dd13701a580f8598c4e7a56d8
Frowsty <daniel.barbis@frostys.tech>
2026-03-10 16:46:11 +0100

    Why is formatting breaking on commit? Attempted fix

--- data/scripts/talkactions/enter_instance.lua
+++ data/scripts/talkactions/enter_instance.lua
@@ -21,8 +21,8 @@ end
 
 local function onSay(player, words, param)
 	if not player:getGroup():getAccess() then
-    return true
-  end
+		return true
+	end
 	if player:getAccountType() < ACCOUNT_TYPE_GOD then
 		return false
 	end
