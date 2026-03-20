5462fbcc713c3d5bd4e94acc7b1e3b64dd728f4a
Tofame <80583148+Tofame@users.noreply.github.com>
2026-03-20 01:31:32 +0100

    apply early return

--- data/scripts/movements/claw_of_the_noxious_spawn.lua
+++ data/scripts/movements/claw_of_the_noxious_spawn.lua
@@ -1,12 +1,14 @@
 local clawOfTheNoxiousSpawn = MoveEvent()
 
 function clawOfTheNoxiousSpawn.onEquip(player, item, slot, isCheck)
-	if isCheck == false then
-		if not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE) then
-			doTargetCombat(0, player, COMBAT_PHYSICALDAMAGE, -150, -200, CONST_ME_DRAWBLOOD)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ouch! The serpent claw stabbed you.")
-			return true
-		end
+	if isCheck then
+		return true
+	end
+
+	if not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE) then
+		doTargetCombat(0, player, COMBAT_PHYSICALDAMAGE, -150, -200, CONST_ME_DRAWBLOOD)
+		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Ouch! The serpent claw stabbed you.")
+		return true
 	end
 	return true
 end
